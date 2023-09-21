ecb69e272aa0be11f7ff195478432a43fac0512e
Hogan <hoganwong88@gmail.com>
2023-09-21 20:31:59 +0800

    most completed except the add minutes exercise

--- src/main/java/com/example/Car.java
+++ src/main/java/com/example/Car.java
@@ -4,20 +4,21 @@ public class Car {
     private int speed;
 
     public Car() {
-        // TODO: Initialize the speed to 0
+        this.speed=0;
     }
 
     public void speedUp(int increment) {
-        // TODO: Increase the speed by the value of the increment parameter
+        this.speed +=increment;
     }
 
     public void slowDown(int decrement) {
-        // TODO: Decrease the speed by the value of the decrement parameter. 
-        // The speed should not be negative.
+        this.speed -=decrement;
+        if (this.speed-decrement<0){
+            this.speed=speed;
+        }
     }
 
     public int currentSpeed() {
-        // TODO: Return the current speed
-        return 0;  // Change this default return statement
+        return this.speed;
     }
 }
